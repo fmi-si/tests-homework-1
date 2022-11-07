26b1ea517ada19471fe1e288d3f60fc094843ac2
Telinc1 <telinc13@gmail.com>
2022-11-07 21:00:00 +0200

    Update testing scripts

--- tests.sh
+++ tests.sh
@@ -1,63 +1,94 @@
 #!/bin/bash
 
-[ -z "$1" ] && exit 1
-
 test_dir=$1
 
-mkdir test-results || exit 1
+if [ ! -d "$test_dir" ]; then
+	echo "\"$1\" is not a test directory." >&2	
+	exit 100
+fi
+
+if ! mkdir test-results; then
+	echo "\"test-results\" directory cannot be created." >&2
+	exit 101
+fi
+
+tasks="$(find "$test_dir/tests" -mindepth 1 -maxdepth 1 -not -name '.*' -type d -printf '%f\n' | sort)"
 
 errors=0
+report="test-results/report.txt"
+echo -n "Tests ran on: " >> $report
+date >> $report
+echo >> $report
 
-for task in $(find "$test_dir" -mindepth 1 -maxdepth 1 -not -name '.*' -type d -printf '%f\n' | sort); do
-    results="test-results/$task.txt"
+for task in $tasks; do
+	results="test-results/$task.txt"
 
-    echo -n "Tests ran on: " >> $results
-    date >> $results
-    echo >> $results
+	echo -n "Tests ran on: " >> $results
+	date >> $results
+	echo >> $results
 
-    echo "Compiler output:" >> $results
-    g++ fn*_d1_$task.cpp -o "$task.out" -std=c++14 -Wpedantic &>> $results
+	correct_tests=0
+	echo "Compiler output:" >> $results
+	g++ fn*_d1_$task.cpp -o "$task.exe" -std=c++14 -Wpedantic &>> $results
 
-    if [ $? -eq 0 ]; then
-        echo >> $results
-        echo "Compilation OK." >> $results
-        echo >> $results
-        echo >> $results
+	if [ $? -eq 0 ]; then
+		echo >> $results
+		echo "Compilation OK." >> $results
+		echo >> $results
+		echo >> $results
 
-        for test in $(find "$test_dir/$task" -mindepth 1 -maxdepth 1 -not -name '.*' -type d -printf '%f\n' | sort); do
-            timeout 3 "./$task.out" < "$test_dir/$task/$test/in" &> "$test_dir/$task/$test/actual"
+		tests_count=$(($(find "$test_dir/tests/$task" | wc -l) / 2))
 
-            echo "Test: $test" >> $results
-            echo "------" >> $results
+		for test in $(seq 1 $tests_count); do
+			temp_file="$(mktemp)"
+			timeout 3 "./$task.exe" < "$test_dir/tests/$task/${test}-in" &> "$temp_file"
 
-            echo "Input:" >> $results
-            cat "$test_dir/$task/$test/in" >> $results
-            echo >> $results
+			if diff -Z "$temp_file" "$test_dir/tests/$task/${test}-out" > /dev/null; then
+				echo "Test \"${test}\": OK" >> $results
+				correct_tests=$((correct_tests+1))
+			else
+				echo "Test \"${test}\": Failed" >> $results
 
-            echo "Expected:" >> $results
-            cat "$test_dir/$task/$test/out" >> $results
-            echo >> $results
+				echo "Input:" >> $results
+				cat "$test_dir/tests/$task/${test}-in" >> $results
+				echo >> $results
 
-            echo "Actual:" >> $results
-            cat "$test_dir/$task/$test/actual" >> $results
-            echo >> $results
+				echo "Expected:" >> $results
+				cat "$test_dir/tests/$task/${test}-out" >> $results
+				echo >> $results
 
-            if diff -Z "$test_dir/$task/$test/actual" "$test_dir/$task/$test/out" > /dev/null; then
-                echo "OK" >> $results
-            else
-                echo "Failed" >> $results
-                errors=$((errors+1))
-            fi
+				echo "Actual:" >> $results
+				head -c 1000 "$temp_file" >> $results
+				echo >> $results
 
-            echo >> $results
-            echo >> $results
-        done
-    else
-        errors=$((errors+1))
+				errors=$((errors+1))
+			fi
 
-        echo >> $results
-        echo "Compilation failed. Skipping tests." >> $results
-    fi
+			echo "____________________" >> $results
+			echo >> $results
+		done
+
+		percentage="$(awk -v correct=$correct_tests -v total=$tests_count 'BEGIN{printf("%.2f", correct * 100 / total)}')"
+		points="$(awk -v cent=$percentage 'BEGIN{printf("%.1f", cent / 100 * 2.5)}')"
+
+		echo "Grade: ${correct_tests}/${tests_count}, ${percentage}%, $points pts." >> $results
+		echo "Task ${task}: ${correct_tests}/${tests_count}, ${percentage}%, $points pts." >> $report
+	else
+		errors=$((errors+1))
+
+		echo >> $results
+		echo "Compilation failed. Skipping tests." >> $results
+		echo "Task ${task}: Does not compile" >> $report
+	fi
 done
+
+echo
+echo
+echo "Check the Summary tab on the left to download the test results."
+echo
+echo
+echo
+
+python3 "${test_dir}/python-test.py" "$test_dir/tests" .
 
 exit $errors
